0455fefb97118f56dda47bf6d48c012a0015596a
MOUSEHACKER1 <63081379+MOUSEHACKER1@users.noreply.github.com>
2020-05-29 22:32:38 -0500

    Update welcome.blade.php

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -93,6 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="http://achedkr.000webhostapp.com/">ACHEDKR</a>
                 </div>
             </div>
         </div>
